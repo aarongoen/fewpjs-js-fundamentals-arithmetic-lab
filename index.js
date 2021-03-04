42c3e1cb236ec82c3bf6366286d9dc0345d299b4
aarongoen <aarongoen2.0@gmail.com>
2021-03-04 16:01:41 -0500

    commit tree d9220719687ebc5a4aaf30c36dff5d184caa8bf0

--- index.js
+++ index.js
@@ -3,14 +3,16 @@
 
 let num1 = 2
 let num2 = 31
-let multiply = function multiplyTo62() {
-    num1 * num2 === 62
-}
+let multiply =
+    num1*num2
 
 
-// let random = (Math.floor(Math.random() * 10) + 1)**(10(Math.floor(Math.random() * 10))) 
-// )
+let random = (Math.floor(Math.random() * 10) + 1)**(10*(Math.floor(Math.random() * 10))) 
 
-// let mod = (num1 % num2 === 4)
 
-let max = 
+let num3 = 16
+let num4 = 6
+let mod = num3%num4
+
+let set = (1, 2, 5, 6, 18, 20)
+let max = Math.max(set)
