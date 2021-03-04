fc2f62c99bb0f0278e159014d2f038fa3356076c
aarongoen <aarongoen2.0@gmail.com>
2021-03-04 11:56:42 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+
+let multiply = function() {
+    num1 * num2 === 62
+}
+
+// let random = (Math.floor(Math.random() * 10) + 1)**(10(Math.floor(Math.random() * 10))) 
+// )
+
+// let mod = (num1 % num2 === 4)
+
+let max = function() {
+    
+}
